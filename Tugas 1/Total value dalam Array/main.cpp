59df56ab6c977d000154839def9e5386c7174729
Aaronnmic <113256747+Aaronnmic@users.noreply.github.com>
2023-03-16 21:01:24 +0700

    Add files via upload

---
+++ Tugas 1/Total value dalam Array/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10], total = 0;
+
+    cout << "Masukkan 10 angka: " << endl;
+
+    for(int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
+        total += arr[i];
+    }
+
+    cout << "Total nilai elemen dalam array: " << total << endl;
+
+    return 0;
+}
